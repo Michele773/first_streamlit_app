94b7bd83721c1ff826b5f8c726f91a9105854b7d
Michele773 <151859741+Michele773@users.noreply.github.com>
2023-11-24 13:19:21 +0100

    Update streamlit_app.py
    add text to first diner app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
-streamlit.title('Hallo mein baba schatzi du bist so süüsssss')
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('und so toll und so ......  ein dickes Bussi')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 
