9a05a70455347040f90148c093690973e6a863dc
Michele773 <151859741+Michele773@users.noreply.github.com>
2023-12-05 11:02:41 +0100

    Update streamlit_app.py
    remove typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + frui_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # remove just unformated text > streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 # zaehlt elemente und header info für den frame 
