84ef088a434f8f4344830b667ebf34f526e6388d
Michele773 <151859741+Michele773@users.noreply.github.com>
2023-12-11 17:41:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,10 +69,10 @@ if streamlit.button('Get Fruit Load List'):
 
 # New Section to display fruityvice api response
 # streamlit.header("Fruityvice Fruit Advice!")
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+#streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur.execute ("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute ("insert into fruit_load_list values ('from streamlit')")
 
 
 
