19f40a9335b34517c56602ad20f4f36bcce86827
Michele773 <151859741+Michele773@users.noreply.github.com>
2023-12-11 15:53:14 +0100

    Update streamlit_app.py
    ff

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
+
+
 streamlit.title('Hallo liebe Leute das ist meine erste Web Applikation!')
 streamlit.header(' My New Healthy Vegan Diner')
    
@@ -34,7 +36,7 @@ try:
      fruit_choice = streamlit.text_input('What fruit would you like information about?')
      if not fruit_choice:
 	       streamlit.error("Please select a stupid fruit you dummy.")
-	 else:
+     else:
 	   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
            streamlit.dataframe(fruityvice_normalized)
